d43912974da03e2d62f928655b7efb887a17eff6
sharanh17 <sharanhidaduggi17@gmail.com>
2024-01-19 11:58:16 +0530

    last update

--- src/App.js
+++ src/App.js
@@ -13,22 +13,28 @@ export default function App() {
 
 
   const handleChange = (e) => {
-  const inputText = e.target.value;
-  setText(inputText);
-
-  const words = inputText.split(" ");
-  const correctedWords = words.map((item) => {
-    const correctedWord = customDictionary[item.toLowerCase()];
-    return correctedWord || item;
-  });
-
-  // correctedWords.join(" ");
-
-  const firstCorrection = correctedWords.find(
-    (word, idx) => word.toLowerCase() !== words[idx].toLowerCase()
-  );
-  setSuggestions(firstCorrection || "");
-};
+    const inputText = e.target.value;
+    setText(inputText);
+  
+    const words = inputText.split(" ");
+    const correctedWords = words.map((item) => {
+      const correctedWord = getCorrectedWord(item);
+      return correctedWord || item;
+    });
+  
+    const firstCorrection = correctedWords.find(
+      (word, idx) => word.toLowerCase() !== words[idx].toLowerCase()
+    );
+  
+    setSuggestions(firstCorrection || "");
+  };
+  
+  const getCorrectedWord = (word) => {
+    // Function to perform case-insensitive correction lookup
+    const lowerCaseWord = word.toLowerCase();
+    return customDictionary[lowerCaseWord];
+  };
+  
 
 
   return (
