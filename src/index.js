725dcb99fb052d0193ab3cb81d1cf8f0ca7060ea
sharanh17 <sharanhidaduggi17@gmail.com>
2024-01-19 12:57:15 +0530

    one last

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SpellCheckApp from './SpellCheckApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SpellCheckApp />
   </React.StrictMode>
 );
 
